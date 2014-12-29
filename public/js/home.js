63abd58b558d22ffca3f2dfeeec3c6f1fef2329f
012-user <fsdf@dfgdf.com>
2014-12-29 17:28:38 +0200

    service

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,10 @@
-angular.module('app').controller('HomeCtrl', function($scope) {
+angular.module('app').factory('getDataForTimeLine', function(){
+  return [
+    { Event:"Zuk is waiking up early", Date:"11/12/2014" },
+    { Event:"Zuk is sleeping", Date:"14/12/2014"}];
+});
+
+angular.module('app').controller('HomeCtrl',function($scope, getDataForTimeLine) {
   $scope.greeting = 'World';
 
   $scope.model = {
@@ -12,4 +18,12 @@ angular.module('app').controller('HomeCtrl', function($scope) {
       alert('Heya, ' + $scope.model.text);
     }
   };
+
+
+  $scope.TimeLineData = getDataForTimeLine;
+  alert(data[0].Event);
+
 });
+
+
+

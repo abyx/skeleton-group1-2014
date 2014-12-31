d589cceeabd3e1ee96b74595a7436beb7dc1b9be
bababa <me@mail.me>
2014-12-31 10:44:16 +0200

    Merge branch 'master' of https://github.com/abyx/skeleton-group1
    Conflicts:
    	public/timeline-portfolio/data.json
    Fix Reloading Timeline

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,7 @@ var mongo = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var express = require('express');
 var bodyParser = require('body-parser');
+var moment = require('moment');
 var app = express();
 
 var db;
@@ -19,6 +20,7 @@ app.post('/example/:id', function(request, response) {
   response.sendStatus(200);
 });
 
+/*
 app.get('/TimeLineData', function(request, response){
   var resJson = {
     timeline:
@@ -78,6 +80,122 @@ app.get('/TimeLineData', function(request, response){
     }
   };
   response.send(resJson);
+});
+*/
+
+app.get('/TimeLineData', function(request, response){
+
+  var resJson = {
+    timeline:
+    {
+      "headline":"באבא זמן",
+      "type":"default",
+      "startDate":"2009,1",
+      "text":"<i><span class='c1'>באבא</span> & <span class='c2'>זמן</span></i>",
+      "asset":
+      {
+        "media":"assets/img/baba_logo.png",
+        "credit":"",
+        "caption":""
+      },
+      "date": [
+        {
+          "startDate":"2013,2,1",
+          "headline":"1/2/2013<br>משיכת כסף בכספומט",
+          "text":"אירוע",
+          "asset": {
+            "media": "assets/img/atm.png"
+          }
+        },
+        {
+          "startDate":"2013,4,2",
+          "headline":"2/4/2013<br>שליחת דואר",
+          "text":"אירוע",
+          "asset": {
+            "media":"assets/img/mail.jpg"
+          }
+        },
+        {
+          "startDate":"2013,5,1",
+          "headline":"פגישה",
+          "text":"אירוע",
+          "asset": {
+            "media":"assets/img/meeting.jpg"
+          }
+        },
+        {
+          "startDate":"2010,2,1",
+          "headline":"1/2/2013<br>משיכת כסף בכספומט",
+          "text":"אירוע",
+          "asset": {
+            "media": "assets/img/atm.png"
+          }
+        },
+        {
+          "startDate":"2009,5,1",
+          "headline":"פגישה",
+          "text":"אירוע",
+          "asset": {
+            "media":"assets/img/meeting.jpg"
+          }
+        }
+      ]
+    }
+  };
+
+  var anotherJson = {
+    timeline:
+    {
+      "headline":"באבא זמן",
+      "type":"default",
+      "startDate":"2012,1",
+      "text":"<i><span class='c1'>נתונים</span> & <span class='c2'>רועננו</span></i>",
+      "asset":
+      {
+        "media":"assets/img/baba_logo.png",
+        "credit":"",
+        "caption":""
+      },
+      "date": [
+        {
+          "startDate":"2014,2,1",
+          "headline":"1/2/2013<br>משיכת כסף בכספומט",
+          "text":"אירוע",
+          "asset": {
+            "media": "assets/img/atm.png"
+          }
+        },
+        {
+          "startDate":"2013,4,2",
+          "headline":"2/4/2013<br>שליחת דואר",
+          "text":"אירוע",
+          "asset": {
+            "media":"assets/img/mail.jpg"
+          }
+        },
+        {
+          "startDate":"2012,5,1",
+          "headline":"פגישה",
+          "text":"אירוע",
+          "asset": {
+            "media":"assets/img/meeting.jpg"
+          }
+        }
+      ]
+    }
+  };
+  console.log(request.query.startDate);
+  console.log(moment(request.query.startDate,'DD/MM/YYYY'));
+  console.log(moment('01/01/2012','DD/MM/YYYY'));
+  console.log(moment(request.query.startDate,'DD/MM/YYYY').isAfter(moment('01/01/2012','DD/MM/YYYY')));
+  if (moment(request.query.startDate,'DD/MM/YYYY').isAfter(moment('01/01/2012','DD/MM/YYYY'))) {
+    response.send(anotherJson);
+  }
+  else
+  {
+    response.send(resJson);
+  }
+
 });
 
 mongo.connect('mongodb://localhost/app', function(err, aDb) {
